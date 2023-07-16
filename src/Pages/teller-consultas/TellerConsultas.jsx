8e7cd941f8f33f07342a40df7d030258d3efd694
Andres Noboa V <aenoboa1@espe.edu.ec>
2023-07-15 22:49:54 -0500

    Parte de Consultas Finalizada

--- src/Pages/teller-consultas/TellerConsultas.jsx
+++ src/Pages/teller-consultas/TellerConsultas.jsx
@@ -1,6 +1,3 @@
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import SearchBar from "./SearchBox";
 import Grid from "@mui/material/Grid";
 import AccountCard from "./AccountCard";
 import TransactionsGrid from "./TransactionsGrid";
@@ -10,52 +7,90 @@ import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import * as React from "react";
 import {useEffect, useState} from "react";
+import {createAPIEndpoint, ENDPOINTS} from "../../api";
+import Typography from "@mui/material/Typography";
+import Grow from '@mui/material/Grow';
 
 export const TellerConsultas = () => {
+    const [value, setValue] = useState("");
+    const [result, setResult] = useState([]);
+    const [checked, setChecked] = useState(false);
+
+    function handleSearch(event) {
+        getInfo(value);
+    }
 
 
-    const [isShown, setIsShown] = useState(false);
+    function getInfo(value) {
+        createAPIEndpoint(ENDPOINTS.accounts)
+            .fetchById(value,
+                {},
+            )
+            .then(async (res) => {
+                setResult(res.data)
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
 
-    const handleClick = event => {
-        setIsShown(current => !current);
+    useEffect(() => {
 
-    };
+    }, []);
+
 
     return (
         <>
             <Grid
                 container
-                spacing={2}
+                spacing={5}
                 direction="column"
                 alignItems="center"
                 sx={{minHeight: '100vh'}}
             >
-                <Grid item>
+
+                <Grid item xs={6}>
                     <Paper
+
                         component="form"
                         sx={{p: '2px 4px', display: 'flex', alignItems: 'center', width: 400}}
                     >
                         <InputBase
                             sx={{ml: 1, flex: 1}}
                             placeholder="Buscar por Numero de Cuenta"
-                            inputProps={{'aria-label': 'search google maps'}}
+                            onChange={event => {                                 //adding the onChange event
+                                setValue(event.target.value)
+                            }}
                         />
-                        <IconButton type="button" sx={{p: '10px'}} aria-label="search">
+                        <IconButton type="button" sx={{p: '10px'}} aria-label="search" onClick={handleSearch}>
                             <SearchIcon/>
                         </IconButton>
                     </Paper>
-
-
                 </Grid>
 
-                <Grid item>
-                    <AccountCard/>
-                </Grid>
-                <Grid item>
-                    <TransactionsGrid/>
-                </Grid>
+                {
+                    // conditional rendering
+                    result.length === 0 ? (
+                        <>
+
+                        </>
+                    ) : (
+                        <>
+                            <Grid item xs={6}>
+                                <AccountCard data={result}/>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Typography>
+                                    Últimos Movimientos
+                                </Typography>
+
+                                <TransactionsGrid/>
+                            </Grid>
+                        </>
+                    )}
 
             </Grid>
+
 
         </>
     )
